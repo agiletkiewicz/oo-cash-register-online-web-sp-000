23ce3384a052c91f18066897a894f96c389b797c
Agi Letkiewicz <a.letkiewicz@gmail.com>
2020-04-30 23:36:13 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -14,7 +14,7 @@ class CashRegister
   
   
   def add_item(item, price, quantity=1)
-    @total += (price * quantity)
+    self.total += (price * quantity)
     counter = 0 
     until counter == quantity
       self.items << item 
